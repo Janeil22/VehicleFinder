d5279f56703b6d71fb133ba2ad8e62fe1f4115ac
mukosman <108299024+mukosman@users.noreply.github.com>
2022-07-19 17:30:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 import requests
 
-model = 'civic'
+def get_data_by_make(make):
+    api_url = 'https://api.api-ninjas.com/v1/cars?make=' + make + '&limit=20'
+    response = requests.get(api_url, headers={'X-Api-Key': 'satQ5cAvuZnDLaHo6QNHhA==be21RIxCh11f66As'})
+    data = response.json()
+    return data
 
-for i in range(10):
-    year = 2010
-    api_url = 'https://api.api-ninjas.com/v1/cars?year={}&limit=30'.format(model)
-    year+=1
-response = requests.get(api_url, headers={'X-Api-Key': 'satQ5cAvuZnDLaHo6QNHhA==be21RIxCh11f66As'})
-data = response.json()
-print(data[0])
-for i in data:
-    print(i['year'])
+def get_data_by_model(model):
+    api_url = 'https://api.api-ninjas.com/v1/cars?model=' + model + '&limit=20'
+    response = requests.get(api_url, headers={'X-Api-Key': 'satQ5cAvuZnDLaHo6QNHhA==be21RIxCh11f66As'})
+    data = response.json()
+    return data
+
+def get_data_by_year(year):
+    api_url = 'https://api.api-ninjas.com/v1/cars?year=' + year + '&limit=20'
+    response = requests.get(api_url, headers={'X-Api-Key': 'satQ5cAvuZnDLaHo6QNHhA==be21RIxCh11f66As'})
+    data = response.json()
+    return data
+
+def get_data_by_all(make, model, year):
+    api_url = 'https://api.api-ninjas.com/v1/cars?make=' + make + '&model=' + model + '&year=' + year + '&limit=20'
+    response = requests.get(api_url, headers={'X-Api-Key': 'satQ5cAvuZnDLaHo6QNHhA==be21RIxCh11f66As'})
+    data = response.json()
+    return data
+
+def getInput():
+    spec = input("Do you want to search make, model, year, or all?")
+    if spec == 'make':
+        make = input('Enter make:')
+        x = [spec, make]
+        return x
+    elif spec == 'model':
+        make = input('Enter make:')
+        x = [spec, make]
+        return x
+
+def getTransmission(data):
+    t = data['transmission']
+    return t 
+
+def getCitympg(data):
+    make = data['make']
+    return make 
+
+def getModel(data):
+    model = data['model']
+    return model
+
+def getMake(data):
+    make = data['make']
+    return make 
