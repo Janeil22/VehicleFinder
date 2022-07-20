1b1612a624f7826812972fb27e5150bf560c7ef8
mukosman <108299024+mukosman@users.noreply.github.com>
2022-07-19 23:33:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,22 +18,28 @@ def get_data_by_year(year):
     data = response.json()
     return data
 
-def get_data_by_all(make, model, year):
-    api_url = 'https://api.api-ninjas.com/v1/cars?make=' + make + '&model=' + model + '&year=' + year + '&limit=20'
+def get_data_by_all(typeList):
+    api_url = 'https://api.api-ninjas.com/v1/cars?make=' + typeList[0] + '&model=' + typeList[1] + '&year=' + typeList[2] + '&limit=20'
     response = requests.get(api_url, headers={'X-Api-Key': 'satQ5cAvuZnDLaHo6QNHhA==be21RIxCh11f66As'})
     data = response.json()
     return data
 
 def getInput():
-    spec = input("Do you want to search make, model, year, or all?")
+    spec = input("Do you want to search make, model, year, or all?"
     if spec == 'make':
         make = input('Enter make:')
-        x = [spec, make]
-        return x
+        return make
     elif spec == 'model':
+        model = input('Enter model')
+        return model
+    elif spec == 'year':
+        year = input('Enter year:')
+        return year
+    elif spec == 'all':
         make = input('Enter make:')
-        x = [spec, make]
-        return x
+        model = input('Enter model')
+        year = input('Enter year:')
+        return [make, model, year]
 
 def getTransmission(data):
     t = data['transmission']
