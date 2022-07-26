1e4cc9db30d9ba843e8fdb8c5343b3d57b87ec12
mukosman <108299024+mukosman@users.noreply.github.com>
2022-07-26 17:26:23 -0500

    presented project

---
+++ website/static/index.js
@@ -0,0 +1,9 @@
+function deleteSearch(searchId) {
+  fetch("/delete-search", {
+    method: "POST",
+    body: JSON.stringify({ searchId: searchId }),
+  }).then((_res) => {
+    window.location.href = "/search";
+  });
+}
+
---
+++ website/views.py
@@ -0,0 +1,74 @@
+from flask import Blueprint, render_template, request, jsonify
+from flask_login import login_required, current_user
+from .models import Search
+from . import db
+from .functions import get_data, get_Citympg, get_Engine, get_Drivetrain, get_Highwaympg, get_class
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        make = request.form.get('make')
+        model = request.form.get('model')
+        year = request.form.get('year')
+        transmission = request.form.get('transmission')
+        data = get_data(make, model, year, transmission)
+        cmg = get_Citympg(data)
+        hmg = get_Highwaympg(data)
+        drive = get_Drivetrain(data)
+        engine = get_Engine(data)
+        c = get_class(data)
+        new_search = Search(
+            make=make, model=model,
+            years=year, transmission=transmission,
+            cmg=cmg, hmg=hmg,
+            drive=drive, engine=engine,
+            c=c, user_id=current_user.id
+        )
+        db.session.add(new_search)
+        db.session.commit()
+
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-search', methods=['POST'])
+def delete_search():
+    search = json.loads(request.data)
+    searchId = search['searchId']
+    search = Search.query.get(searchId)
+    if search:
+        if search.user_id == current_user.id:
+            db.session.delete(search)
+            db.session.commit()
+
+    return jsonify({})
+
+@views.route('/search', methods=['GET', 'POST'])
+@login_required
+def search():
+    if request.method == 'POST':
+        make = request.form.get('make')
+        model = request.form.get('model')
+        year = request.form.get('year')
+        transmission = request.form.get('transmission')
+        data = get_data(make, model, year, transmission)
+        cmg = get_Citympg(data)
+        hmg = get_Highwaympg(data)
+        drive = get_Drivetrain(data)
+        engine = get_Engine(data)
+        c = get_class(data)
+        new_search = Search(
+            make=make, model=model,
+            years=year, transmission=transmission,
+            cmg=cmg, hmg=hmg,
+            drive=drive, engine=engine,
+            c=c, user_id=current_user.id
+        )
+        db.session.add(new_search)
+        db.session.commit()
+    return render_template("search.html", user=current_user)
+
