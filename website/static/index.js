b745b5f7c0dfe5fd5a87e38691573100f9c124d5
mukosman <108299024+mukosman@users.noreply.github.com>
2022-07-26 17:24:34 -0500

    Delete website directory

--- website/static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteSearch(searchId) {
-  fetch("/delete-search", {
-    method: "POST",
-    body: JSON.stringify({ searchId: searchId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
-}
